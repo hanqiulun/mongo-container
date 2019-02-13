3c99f49fa7e2d1fd9942568dc28460acbdfe7f82
hanqiulun <linhanqiu1123@gmail.com>
2019-02-13 16:52:11 +0800

    add test

---
+++ mongo-shard-kubernetes/scripts/test.js
@@ -0,0 +1,15 @@
+
+sh.status();
+var db = db.getSiblingDB( 'config' );
+db.settings.save({'_id':'chunksize','value':1});
+var db = db.getSiblingDB( 'python' );
+for(i=1;i<=50000;i++){db.user.insert({'id':i,'name':'jack'+i})};
+sh.enableSharding('python');
+db.user.createIndex({"id":1});
+sh.shardCollection('python.user',{"id":1});
+sh.status();
+
+
+
+
+sh.status(); var db = db.getSiblingDB( 'config' ); db.settings.save({'_id':'chunksize','value':1}); var db = db.getSiblingDB( 'python' ); for(i=1;i<=50000;i++){db.user.insert({'id':i,'name':'jack'+i})}; sh.enableSharding('python'); db.user.createIndex({"id":1}); sh.shardCollection('python.user',{"id":1}); sh.status();
