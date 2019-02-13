fff394100143902d92e9862c602164954090e95e
hanqiulun <linhanqiu1123@gmail.com>
2019-02-13 17:05:02 +0800

    fix

--- mongo-shard-kubernetes/scripts/test.sh
+++ mongo-shard-kubernetes/scripts/test.sh
@@ -3,4 +3,4 @@
 POD_NAME=$(kubectl get pods | grep "mongos1" | awk '{print $1;}')
 
 mongo_command=$(cat test.js)
-kubectl exec -it $POD_NAME -- bash -c "mongo --eval "$mongo_command""
+kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$mongo_command'"
