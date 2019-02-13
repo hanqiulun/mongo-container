89b81e3e86d3f23be5a88efe303bd72dff3a060f
hanqiulun <linhanqiu1123@gmail.com>
2019-02-13 16:17:45 +0800

    fix

--- mongo-shard-kubernetes/scripts/test.sh
+++ mongo-shard-kubernetes/scripts/test.sh
@@ -5,6 +5,7 @@ POD_NAME=$(kubectl get pods | grep "mongos1" | awk '{print $1;}')
 
 CMDS=("use config",'db.settings.save({"_id":"chunksize","value":1})',"use python",'for(i=1;i<=50000;i++){db.user.insert({"id":i,"name":"jack"+i})}','sh.enableSharding("python")','db.user.createIndex({"id":1})','sh.shardCollection("python.user",{"id":1})',"sh.status()")
 for CMD in ${array[@]}
-    do
-    kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$CMD'"
-    done
+do
+echo $CMD
+kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$CMD'"
+done
