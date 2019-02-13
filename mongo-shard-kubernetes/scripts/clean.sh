e2294321d69537271dfd3522dc7be8f82d18e526
hanqiulun <linhanqiu1123@gmail.com>
2019-02-13 16:15:32 +0800

    fix

--- mongo-shard-kubernetes/scripts/clean.sh
+++ mongo-shard-kubernetes/scripts/clean.sh
@@ -16,3 +16,9 @@ done
 
 echo -e "\nDeleting router nodes"
 kubectl delete -f $BASE"mongos.yaml"
+
+echo -e "\nDeleting shard yaml"
+
+for ((rs=1; rs<=$SHARD_REPLICA_SET; rs++)) do
+    rm -rf $BASE"mongo_sh_"$rs".yaml"
+done
---
+++ mongo-shard-kubernetes/scripts/test.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+POD_NAME=$(kubectl get pods | grep "mongos1" | awk '{print $1;}')
+
+CMDS=("use config",'db.settings.save({"_id":"chunksize","value":1})',"use python",'for(i=1;i<=50000;i++){db.user.insert({"id":i,"name":"jack"+i})}','sh.enableSharding("python")','db.user.createIndex({"id":1})','sh.shardCollection("python.user",{"id":1})',"sh.status()")
+for CMD in ${array[@]}
+    do
+    kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$CMD'"
+    done
