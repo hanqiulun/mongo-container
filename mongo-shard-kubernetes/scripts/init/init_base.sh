9a532dc910a4320db20bd13af05b659c33ea463b
hanqiulun <hanqiulun1123@gmail.com>
2019-02-15 16:46:44 +0800

    fix

---
+++ mongo-shard-kubernetes/scripts/init/init_base.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+source scripts/config
+
+BASE="sources/"
+
+# create secret keyfile
+openssl rand -base64 64 > keyfile
+chmod 600 keyfile
+
+kubectl create secret generic keyfile --from-file=keyfile
+
+
+
+#Creating config nodes
+kubectl create -f  $BASE"mongo_config.yaml"
+
+#Waiting for containers
+echo "Waiting config containers"
+kubectl get pods | grep "mongocfg" | grep "ContainerCreating"
+while [ $? -eq 0 ]
+do
+  sleep 1
+  echo -e "\n\nWaiting the following containers:"
+  kubectl get pods | grep "mongocfg" | grep "ContainerCreating"
+done
+
+sleep 5
+
+#Initializating configuration nodes
+POD_NAME=$(kubectl get pods | grep "mongocfg1" | awk '{print $1;}')
+echo "Initializating config replica set... connecting to: $POD_NAME"
+CMD='rs.initiate({ _id : "cfgrs", configsvr: true, members: [{ _id : 0, host : "mongocfg1:27019" },{ _id : 1, host : "mongocfg2:27019" },{ _id : 2, host : "mongocfg3:27019" }]})'
+kubectl exec -it $POD_NAME -- bash -c "mongo --port 27019 --eval '$CMD'"
+
+
+#Initializing routers
+kubectl create -f $BASE"mongos.yaml"
+echo "Waiting router containers"
+kubectl get pods | grep "mongos[0-9]" | grep "ContainerCreating"
+while [ $? -eq 0 ]
+do
+  sleep 1
+  echo -e "\n\nWaiting the following containers:"
+  kubectl get pods | grep "mongos[0-9]" | grep "ContainerCreating"
+done
+
+#create root user
+POD_NAME=$(kubectl get pods | grep "mongos1" | awk '{print $1;}')
+mongo_command=$(cat scripts/init/create_user.js)
+kubectl exec -it $POD_NAME -- bash -c "mongo -u root -p --eval '$mongo_command'"
+
+
+echo -e "\033[32m All done!!! \033[0m"
+
+
+
+
---
+++ mongo-shard-kubernetes/scripts/init/init_shard.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+source scripts/config
+source scripts/volume_op.sh
+source scripts/brick_op.sh
+
+SHARDTEMP="templates/mongo_sh_x.yaml"
+BASE="sources/"
+
+#Creating shard nodes
+
+
+function generate_depand()
+    {
+        create_dir $1
+        create_volume $1
+    }
+function generate_sh_yaml()
+    {
+      new_file_name=$BASE"mongo_sh_$1.yaml"
+      cp $SHARDTEMP $new_file_name
+      sed -i "s/mongoshx/mongosh$1/g" $new_file_name
+      sed -i "s/rsx/rs$1/g" $new_file_name
+      sed -i "s/mongox/mongo$1/g" $new_file_name
+      kubectl create -f  $BASE"mongo_sh_"$rs".yaml"
+    }
+
+function add_shard()
+    {
+        #Waiting for containers
+        POD_STATUS= kubectl get pods | grep "mongosh" | grep "ContainerCreating"
+        echo "Waiting shard containers"
+        kubectl get pods | grep "mongosh" | grep "ContainerCreating"
+        while [ $? -eq 0 ]
+        do
+        sleep 1
+        echo -e "\n\nWaiting the following containers:"
+        kubectl get pods | grep "mongosh" | grep "ContainerCreating"
+        done
+        # Initializating shard nodes
+        POD_NAME=$(kubectl get pods | grep "mongosh$rs-1" | awk '{print $1;}')
+        echo "Pod Name: $POD_NAME"
+        CMD="rs.initiate({ _id : \"rs$rs\", members: [{ _id : 0, host : \"mongosh$rs-1:27017\" },{ _id : 1, host : \"mongosh$rs-2:27017\" },{ _id : 2, host : \"mongosh$rs-3:27017\" }]})"
+        #Executing cmd inside pod
+        echo $CMD
+        kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$CMD'"
+        # #Adding shard to cluster
+        POD_NAME=$(kubectl get pods | grep "mongos1" | awk '{print $1;}')
+        CMD="sh.addShard(\"rs$rs/mongosh$rs-1:27017\")"
+        #Executing cmd inside pod
+        echo $CMD
+        kubectl exec -it $POD_NAME -- bash -c "mongo --eval '$CMD'"
+    }
+
+function init_shard()
+    {
+        generate_sh_yaml $1
+        add_shard $1
+    }
+
+
+
+init_shard $1
+
