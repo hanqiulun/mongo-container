320159e6082e895f357f18158b96fae3eb679803
hanqiulun <hanqiulun1123@gmail.com>
2019-02-21 11:24:47 +0800

    update role

--- mongo-shard-kubernetes/scripts/init/create_user.js
+++ mongo-shard-kubernetes/scripts/init/create_user.js
@@ -6,6 +6,32 @@ db.createUser(
         roles:
             [
                 { role: "root", db: "admin" },
+            ]
+    })
+db.createUser(
+    {
+        user: "clusterAdmin",
+        pwd: "clusterAdmin",
+        roles:
+            [
                 { role: "clusterAdmin", db: "admin" }
+            ]
+    })
+db.createUser(
+    {
+        user: "dbAdminAnyDatabase",
+        pwd: "dbAdminAnyDatabase",
+        roles:
+            [
+                { role: "dbAdminAnyDatabase", db: "admin" }
+            ]
+    })
+db.createUser(
+    {
+        user: "userAdminAnyDatabase",
+        pwd: "userAdminAnyDatabase",
+        roles:
+            [
+                { role: "userAdminAnyDatabase", db: "admin" }
             ]
     })
